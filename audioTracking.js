7a522094382e231fd2b690f8f3b9df40d6294d7e
alwestmo <30701056+alwestmo@users.noreply.github.com>
2020-03-31 17:34:31 -0400

    Update audioTracking.js

--- audioTracking.js
+++ audioTracking.js
@@ -2,26 +2,34 @@ jQuery(document).ready(function(){
 
   jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.played", function(event, index){
     console.log(index);
-    console.log("Looking for this PLAYED event");
-  });
+    const mediaSession = new MediaSession(
+    mParticle,                    // mParticle SDK Instance
+    '000111',                    // Custom Media ID
+    'Westworld Audio Content',   // Custom Media Title
+    95000,                       // Duration in milliseconds
+    'Audio',                      // Content Type (Video or Audio)
+    'OnDemand'                    // Stream Type (OnDemand, Live, etc.)
+    true                          // Log Page Event Toggle (true/false)
+    )
+});
 
   jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.paused", function(event, index){
     console.log(index);
-  });
+});
 
   jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.stopped", function(event, index){
     console.log(index);
-  });
+});
 
   jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.playprogress", function(event, data){
     console.log(data);
     console.log(data.current);
     console.log(data.duration);
-  });
+});
 
   jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.switched", function(event, data){
     console.log(data);
     console.log(data.previous);
     console.log(data.current);
-  });
+});
 });
