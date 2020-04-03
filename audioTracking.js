1a1a35fff706af0d2a74e4e7a4257c54f98511e3
alwestmo <30701056+alwestmo@users.noreply.github.com>
2020-04-03 14:37:55 -0400

    Adding Session Variable

--- audioTracking.js
+++ audioTracking.js
@@ -1,37 +1,41 @@
 jQuery(document).ready(function(){
+    var sessionStarted = false;
 
-  jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.played", function(event, index){
-    console.log(index);
-    mediaSDK.logPlay({
-        customAttributes: {
-            mediaType: 'Audio'
-        }
+    jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.played", function(event, index){
+        console.log(index);
+        mediaSDK.logSessionStart();
+        sessionStarted = true;
+        mediaSDK.logPlay({
+            customAttributes: {
+                mediaType: 'Audio'
+            }
+        });
+    });
+
+    jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.paused", function(event, index){
+        console.log(index);
+        mediaSDK.logPause({
+            customAttributes: {
+                currentPlayheadPosition: data.current,
+                exampleAttribute: 'I HAZ PAWZ'
+            }
+        });
+    });
+
+    jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.stopped", function(event, index){
+        console.log(index);
+    });
+
+    jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.playprogress", function(event, data){
+        console.log(data);
+        console.log(data.current);
+        console.log(data.duration);
+    });
+
+    jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.switched", function(event, data){
+        console.log(data);
+        console.log(data.previous);
+        console.log(data.current);
     });
 });
 
-  jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.paused", function(event, index){
-    console.log(index);
-    mediaSDK.logPause({
-        customAttributes: {
-            currentPlayheadPosition: data.current,
-            exampleAttribute: 'I HAZ PAWZ'
-        }
-    });
-});
-
-  jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.stopped", function(event, index){
-    console.log(index);
-});
-
-  jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.playprogress", function(event, data){
-    console.log(data);
-    console.log(data.current);
-    console.log(data.duration);
-});
-
-  jQuery("#amazingaudioplayer-1").bind("amazingaudioplayer.switched", function(event, data){
-    console.log(data);
-    console.log(data.previous);
-    console.log(data.current);
-});
-});
