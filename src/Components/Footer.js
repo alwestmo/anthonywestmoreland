0e4cd60cae6b8a1045712c683d076ca540f124f4
Anthony Westmoreland <anthony@anthonys-mbp.lan>
2020-07-05 17:31:39 -0400

    making changes to content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -14,9 +14,6 @@ class Footer extends Component {
 
      <div className="row">
         <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
 
            <ul className="copyright">
               <li>&copy; Copyright 2017 Nordic Giant</li>
